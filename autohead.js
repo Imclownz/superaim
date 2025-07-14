2377ea27b9ffbde8d674d5332896088b18992979
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:18:43 +0700

    autohead.js

--- autohead.js
+++ autohead.js
@@ -1,14 +1,15 @@
-// Enhanced Auto Headshot Script
+// Advanced Auto Headshot - Stability + Extended Hitbox
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
-  data.headshotBias = 1.5;
+  data.headshotBias = 1.8;
   data.autoTargetHead = true;
   data.bulletCurve = "linear";
-  data.hitRateBoost = 1.25;
+  data.hitRateBoost = 1.35;
   data.aimCorrection = true;
   data.headHitboxEnlarged = true;
+  data.predictiveLineup = true;
   body = JSON.stringify(data);
 } catch (e) {
   console.log("Auto headshot error:", e);
