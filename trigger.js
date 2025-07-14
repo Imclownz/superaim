0bde26bd21764cecc8ff8446572dfe17834c18c2
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:21:51 +0700

    trigger.js

--- trigger.js
+++ trigger.js
@@ -1,12 +1,12 @@
-// Enhanced Trigger Bot Script
+// Trigger Bot - Fast Reactive Fire
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
   data.triggerBot = true;
-  data.triggerDelay = 60;
+  data.triggerDelay = 35;
   data.fireOnSight = true;
-  data.reactionTime = "fast";
+  data.reactionTime = "ultra-fast";
   data.targetValidation = true;
   body = JSON.stringify(data);
 } catch (e) {
