34172b15940591a656d03b47a0550312f3169949
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:22:20 +0700

    udp.js

--- udp.js
+++ udp.js
@@ -1,13 +1,14 @@
-// Enhanced UDP Optimization Script
+// UDP Accelerated Gaming Mode
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
   data.useUDP = true;
-  data.udpPriority = 1;
-  data.udpBufferSize = 1024;
+  data.udpPriority = 2;
   data.udpKeepAlive = true;
   data.udpAcceleration = true;
+  data.packetOrderFix = true;
+  data.socketBufferTune = true;
   body = JSON.stringify(data);
 } catch (e) {
   console.log("UDP Boost error:", e);
