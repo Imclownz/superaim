aec7b37fdb8dfd4dd626e41e77ac0c3d1d7b457f
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:22:46 +0700

    weapon.js

--- weapon.js
+++ weapon.js
@@ -1,13 +1,13 @@
-// Smart Weapon AI Script
+// Smart Weapon Detection AI v2
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
   data.weaponAIEnabled = true;
   data.smartDetection = true;
-  data.autoConfig = true;
-  data.weaponRecoilProfile = "adaptive";
-  data.autoSwitchLogic = true;
+  data.weaponTypeAdapt = true;
+  data.recoilProfileSync = true;
+  data.autoAdjustHandling = true;
   body = JSON.stringify(data);
 } catch (e) {
   console.log("Weapon AI error:", e);
