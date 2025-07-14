f1420b5eba07ba86bb217b2a85f00d133ac2afed
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:17:58 +0700

    aimbot.js

--- aimbot.js
+++ aimbot.js
@@ -1,16 +1,18 @@
-// Enhanced Aimbot Script for Free Fire
+// Ultra Aimbot - Precision + Speed + Predictive Tracking
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
   data.autoAim = true;
-  data.aimAssistLevel = "pro";
-  data.aimSensitivity = 1.2;
-  data.aimFov = 30;
+  data.aimAssistLevel = "elite";
+  data.aimSensitivity = 1.45;
+  data.aimFov = 35;
   data.aimSnap = true;
   data.aimTarget = "head";
-  data.aimSmoothness = 0.85;
+  data.aimSmoothness = 0.9;
   data.aimPredictMovement = true;
+  data.aimAcceleration = true;
+  data.latencyCompensation = true;
   body = JSON.stringify(data);
 } catch (e) {
   console.log("Aimbot error:", e);
