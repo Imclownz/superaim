af7276254fe88f079464de6d77e9169f92b8cabc
Imclownz <namlevan.ok@gmail.com>
2025-07-14 23:20:29 +0700

    ping.js

--- ping.js
+++ ping.js
@@ -1,14 +1,15 @@
-// Latency Optimization Script
+// Network Optimizer - Ultra Low Ping Simulation
 let body = $response.body;
 
 try {
   let data = JSON.parse(body);
-  data.latency = 15;
+  data.latency = 5;
+  data.packetLoss = 0;
   data.networkType = "udp";
-  data.packetDelay = 0;
-  data.packetLossCompensation = true;
+  data.packetJitter = 0;
   data.jitterControl = true;
-  data.retransmissionControl = true;
+  data.packetFlush = true;
+  data.priorityMode = "realtime";
   body = JSON.stringify(data);
 } catch (e) {
   console.log("Ping optimization error:", e);
